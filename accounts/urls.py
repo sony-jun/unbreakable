5a022d4bc0bff7c363da105560201341e2b95c4e
jhkim9028 <jhkim9028@gmail.com>
2022-11-28 11:53:08 +0900

    feat:회원 정보 수정, 프로필 페이지 추가 [김재형]

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
+    path('<int:user_pk>/update/', views.update, name="update"),
+    path('<int:user_pk>/profile/', views.profile, name="profile"),
 ]
