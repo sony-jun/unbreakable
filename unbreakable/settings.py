7a045f5704964669d19e89ecb1550bb9d8c716b0
sony-jun <warrise@naver.com>
2022-12-09 09:45:08 +0900

    설정

--- unbreakable/settings.py
+++ unbreakable/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
