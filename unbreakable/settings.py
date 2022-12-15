64bd617bb5836c22f4dbda1fa3bcffb7e3ee15c1
sony-jun <warrise@naver.com>
2022-12-15 19:33:53 +0900

    youtube_key delete

--- unbreakable/settings.py
+++ unbreakable/settings.py
@@ -171,7 +171,7 @@ AUTH_USER_MODEL = "accounts.User"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-YOUTUBE_DATA_API_KEY = "AIzaSyDmM8FkPClJwXWbnF3-OMHJSxo5Ia5gbP8"
+YOUTUBE_DATA_API_KEY = ""
 
 
 DEBUG = os.getenv("DEBUG") == "True"
@@ -208,24 +208,3 @@ else:
             "PORT": "5432",
         }
     }
-
-
-PWA_APP_NAME = "지금 우린"
-PWA_APP_DESCRIPTION = "Music diary"
-PWA_APP_THEME_COLOR = "#0A0302"
-PWA_APP_BACKGROUND_COLOR = "#ffffff"
-PWA_APP_DISPLAY = "standalone"
-PWA_APP_SCOPE = "/"
-PWA_APP_ORIENTATION = "any"
-PWA_APP_START_URL = "/"
-PWA_APP_STATUS_BAR_COLOR = "default"
-PWA_APP_ICONS = [{"src": "/static/images/favicon.png", "sizes": "160x160"}]
-PWA_APP_ICONS_APPLE = [{"src": "/static/images/favicon.png", "sizes": "160x160"}]
-PWA_APP_SPLASH_SCREEN = [
-    {
-        "src": "/static/images/favicon.png",
-        "media": "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
-    }
-]
-PWA_APP_DIR = "ltr"
-PWA_APP_LANG = "en-US"
